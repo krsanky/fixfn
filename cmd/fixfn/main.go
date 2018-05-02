2de65c76664ad28f5fb7a96f57363139bffa104f
paul wisehart <paul@oldcode.org>
2018-05-02 11:03:41 -0400

    asd

--- cmd/fixfn/main.go
+++ cmd/fixfn/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/krsanky/fixfn"
 )
 
 func main() {
-	for i, a := range os.Args[1:] {
-		fmt.Printf("%d:%s ", i, a)
+	//print_args()
+	if len(os.Args[1:]) < 1 {
+		usage()
+	} else {
+		dir := os.Args[1]
+		fixfn.Inspect(dir)
 	}
-	fmt.Println()
+}
 
-	fmt.Printf("Hey!\n")
+func print_args() {
+	//fmt.Printf("len args: %d\n", len(os.Args[1:]))
+	for i, a := range os.Args[1:] {
+		fmt.Printf("%d:%s \n", i, a)
+	}
+}
+
+func usage() {
+	fmt.Printf("%s <dir>\n", os.Args[0])
 }
